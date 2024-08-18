bc5162b637d37bc5a978af81a84fcf37dce41bf6
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-08-17 22:55:39 -0300

    feat: creating login method on auth client

--- src/main/java/dev/vini/spotify_api/client/AuthSpotifyClient.java
+++ src/main/java/dev/vini/spotify_api/client/AuthSpotifyClient.java
@@ -1,6 +1,9 @@
 package dev.vini.spotify_api.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
         name = "AuthSpotifyClient",
@@ -8,4 +11,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 )
 public interface AuthSpotifyClient {
 
+    @PostMapping(value = "/api/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    LoginResponse login(@RequestBody LoginRequest loginRequest);
 }
