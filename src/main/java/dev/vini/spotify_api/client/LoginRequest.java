84453673d80df1da605274eaaef5de665805994a
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-08-17 22:56:29 -0300

    feat: adding json SnakeCase annotation

--- src/main/java/dev/vini/spotify_api/client/LoginRequest.java
+++ src/main/java/dev/vini/spotify_api/client/LoginRequest.java
@@ -1,5 +1,9 @@
 package dev.vini.spotify_api.client;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class LoginRequest {
 
     private String grantType;
