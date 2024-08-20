6a3467f0c1daeafaea924bbff9f64f051a86ec4b
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-08-19 22:52:12 -0300

    feat: creating endpoint to get access token

--- src/main/java/dev/vini/spotify_api/controller/ShowController.java
+++ src/main/java/dev/vini/spotify_api/controller/ShowController.java
@@ -1,6 +1,10 @@
 package dev.vini.spotify_api.controller;
 
 import dev.vini.spotify_api.client.AuthSpotifyClient;
+import dev.vini.spotify_api.client.LoginRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +17,16 @@ public class ShowController {
     public ShowController(AuthSpotifyClient authSpotifyClient) {
         this.authSpotifyClient = authSpotifyClient;
     }
+
+    @GetMapping("/shows")
+    public ResponseEntity<String> login(){
+        var request = new LoginRequest(
+                "client_credentials",
+                "your_client_id",
+                "your_client_secret"
+        );
+
+        return ResponseEntity.ok(authSpotifyClient.login(request).getAccessToken());
+    }
+
 }
