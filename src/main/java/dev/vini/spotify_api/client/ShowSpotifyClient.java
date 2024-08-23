78cd2cca335d081e1ed2ad59e8040cb15fe88fb2
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-08-22 22:17:14 -0300

    feat: adding ids parameter on show client method

--- src/main/java/dev/vini/spotify_api/client/ShowSpotifyClient.java
+++ src/main/java/dev/vini/spotify_api/client/ShowSpotifyClient.java
@@ -3,6 +3,7 @@ package dev.vini.spotify_api.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         name = "ShowSpotifyClient",
@@ -11,5 +12,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 public interface ShowSpotifyClient {
 
     @GetMapping("v1/shows")
-    ShowResponse getShows(@RequestHeader("Authorization") String authorization);
+    ShowResponse getShows(@RequestHeader("Authorization") String authorization,
+                          @RequestParam String ids);
 }
